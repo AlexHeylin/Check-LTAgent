1e0ea6db77f0619bfbbb9d5bf829eb19b27f5973
Alex Heylin <AlexH@GMAL.CO.UK>
2018-12-09 04:47:31 +0000

    Add logging severity

--- Check-LTAgent.ps1
+++ Check-LTAgent.ps1
@@ -15,7 +15,7 @@
 ## 2018-12-06 Rework the services section to be more forgiving and retry changes to avoid unnecessary reinstalls.
 ## 2018-12-08 Force logging on and truncate log automatically, to debug with unexpected reinstalls.
 ## 2018-12-08 Fix service start type detection for older PoSh.
-## 2018-12-09 Add centralised logging & various reliability improvements
+## 2018-12-09 Add centralised logging & log severities, also various reliability improvements. 
 
 ## If you want to set default / override values, do that here
 ## $LTSrv = "labtech.mymspname.here"
@@ -125,204 +125,204 @@ function outlog {
 
 ## Do the needful if unavoidable
 Function Reinstall {
-	outlog "Starting Reinstall function"
+	outlog "Starting Reinstall function" "DEBUG"
 
-	outlog "Trying to repair LTAgent first..."
+	outlog "Trying to repair LTAgent first..." "DEBUG"
 	## Dynamically load the latest LT PoSh module
-	outlog "Dynamically loading LT-PoSh module from Github"
+	outlog "Dynamically loading LT-PoSh module from Github" "DEBUG"
 	try {
 		(new-object Net.WebClient).DownloadString('http://bit.ly/LTPoSh') | iex ; 
 	} catch {
 		$ErrorMessage = $_.Exception.Message
-		outlog "EXCEPTION: $ErrorMessage"
+		outlog "EXCEPTION: $ErrorMessage" "ERROR"
 		throw
 	}
 
 	if (((('LTService') | Get-Service -EA 0 | Measure-Object | Select-Object -Expand Count) -eq 0) -or ((('LTSvcMon') | Get-Service -EA 0 | Measure-Object | Select-Object -Expand Count) -eq 0)) {
-		outlog "Services found - Calling Restart-LTService"
+		outlog "Services found - Calling Restart-LTService" "DEBUG"
 		try {
 			$startResult = Restart-LTService
 			## NOTE: Do NOT rely on the text of the result. It can lie and say services started when they LTService did not start. 
 			## TODO: Try and fix & submit change to LT-PoSh for this.
 		} catch {
 			$ErrorMessage = $_.Exception.Message
-			outlog "EXCEPTION: $ErrorMessage"	
-			outlog "Result of Restart-LTService was $startResult"
+			outlog "EXCEPTION: $ErrorMessage" "ERROR"
+			outlog "Result of Restart-LTService was $startResult" "DEBUG"
 			throw
 		}
 	} else {
-		outlog "At least one service not found"
+		outlog "At least one service not found" "ERROR"
 	}
 	
 	if (((Get-Service LTService -EA 0 -WA 0 ).Status -ne "Running") -or ((Get-Service LTSvcMon -EA 0 -WA 0 ).Status -ne "Running")) {
-		outlog "Services didn't start properly, proceeding with reinstall"
+		outlog "Services didn't start properly, proceeding with reinstall" "WARN"
 		## Check TerminalServerMode using WMI.  1 = Application Server mode (Terminal Server), 0 = Remote Administration mode (normal RDP)
 		if ( (Get-WmiObject -Namespace "root\CIMV2\TerminalServices" -Class "Win32_TerminalServiceSetting" ).TerminalServerMode -eq 1) {
-			outlog "Terminal server detected. Switching it to INSTALL mode"
+			outlog "Terminal server detected. Switching it to INSTALL mode" "DEBUG"
 			$result = iex "$env:windir\system32\CHANGE USER /INSTALL"
-			outlog "Result was $result"
+			outlog "Result was $result" "DEBUG"
 		}
 
 
 		## OK, we're going to have to reinstall.
-		outlog "We're going to have to get more brutal and try a (re)install. Taking backup of settings first"
+		outlog "We're going to have to get more brutal and try a (re)install. Taking backup of settings first" "WARN"
 		$backupResult = New-LTServiceBackup
-		outlog "Result of New-LTServiceBackup was $backupResult"
+		outlog "Result of New-LTServiceBackup was $backupResult" "DEBUG"
 
 		## Use any existing LocationID unless $forceLocID set
 		## This allows agents to REinstall to the location they're already in LT, even if cmd line params use a different default site.
 		## Stops agents moving back to "main" location if they've been manually moved to another location in LT.
 		If (($forceLocID -ne $true) -and ((get-LTServiceInfo).LocationID -ge 1)){
-			outlog "Found existing LocationID and forceLocID not set so using it"
+			outlog "Found existing LocationID and forceLocID not set so using it" "DEBUG"
 			$LTLoc = (get-LTServiceInfo).LocationID
 		}
 
-		outlog "Calling Reinstall-LTService -Server $LTSrv -LocationID $LTLoc"
+		outlog "Calling Reinstall-LTService -Server $LTSrv -LocationID $LTLoc" "DEBUG"
 		## Call the module to do the (re)install
 		#Reinstall-LTService -Server $LTSrv -LocationID $LTLoc
 		$InstallResult = Reinstall-LTService -Server $LTSrv -LocationID $LTLoc >> $LogFile
-		outlog "Result of Reinstall-LTService was $InstallResult"
+		outlog "Result of Reinstall-LTService was $InstallResult" "DEBUG"
 
 		## Check TerminalServerMode using WMI.  1 = Application Server mode (Terminal Server), 0 = Remote Administration mode (normal RDP)
 		if ( (Get-WmiObject -Namespace "root\CIMV2\TerminalServices" -Class "Win32_TerminalServiceSetting" ).TerminalServerMode -eq 1) {
-			outlog "Terminal server detected. Switching it to EXECUTE mode"
+			outlog "Terminal server detected. Switching it to EXECUTE mode" "DEBUG"
 			$result = iex "$env:windir\system32\CHANGE USER /EXECUTE"
-			outlog "Result was $result"
+			outlog "Result was $result" "DEBUG"
 		}
 	} else {
-		outlog "Services started OK so we avoided reinstall :-)"
+		outlog "Services started OK so we avoided reinstall :-)" "DEBUG"
 	}
-	outlog "Finished reinstall function"
+	outlog "Finished reinstall function" "DEBUG"
 }
 
 
 ## Main Flow
 If ($LTSrv -eq "labtech.mymspname.here" -or $LTSrv -eq "" -or $LTSrv -eq $null) { 
-	outlog "You need to specify the LT server to use by setting `$LTSrv variable before calling this script" ;	
+	outlog "You need to specify the LT server to use by setting `$LTSrv variable before calling this script" "CRITICAL";	
 } else {
 
-    outlog "Checking health of LabTech agent" ;
+    outlog "Checking health of LabTech agent" "DEBUG";
 
     if ( ((Get-ItemProperty -Path HKLM:\SOFTWARE\LabTech\Service -Name "ID" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).ID -eq $null) `
 		    -or ((Get-ItemProperty -Path HKLM:\SOFTWARE\LabTech\Service\Settings  -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).ServerAddress -notlike "*" + $LTSrv)
 	    )  
     { 
-        outlog "The LT agent registry keys do not look right - going to reinstall"
+        outlog "The LT agent registry keys do not look right - going to reinstall" "ERROR"
 	    Reinstall
     } else {
-	    outlog "Registry checks OK. Checking services exist"
+	    outlog "Registry checks OK. Checking services exist" "DEBUG"
 		if ((('LTService') | Get-Service -EA 0 | Measure-Object | Select-Object -Expand Count) -eq 0) {
-			outlog "LTService is missing. Calling reinstall."
+			outlog "LTService is missing. Calling reinstall." "WARN"
 			Reinstall
 		} else {
-			outlog "LTservice is installed"
+			outlog "LTservice is installed" "DEBUG"
 		}
 
 		if ((('LTSvcMon') | Get-Service -EA 0 | Measure-Object | Select-Object -Expand Count) -eq 0) {
-			outlog "LTSvcMon is missing. Calling reinstall."
+			outlog "LTSvcMon is missing. Calling reinstall." "WARN"
 			Reinstall
 		} else {
-			outlog "LTSvcMon is installed"
+			outlog "LTSvcMon is installed" "DEBUG"
 		}		
-		outlog "It seems LT is installed - checking that services look OK."
+		outlog "It seems LT is installed - checking that services look OK." "INFO"
 		
-	    outlog "Checking LTService is set to Auto start"
+	    outlog "Checking LTService is set to Auto start" "DEBUG"
 		## It seems sometime script just dies soon after here. No exception is thrown
 		If (((Get-WmiObject -Class Win32_Service -Property StartMode -Filter "Name='LTService'").StartMode) -ne "Auto") { 
-			outlog "LTService is not set to Auto start. Attempting to set it to Auto"
+			outlog "LTService is not set to Auto start. Attempting to set it to Auto" "WARN"
 			Try {
 				Set-Service LTService -startupType Automatic -ErrorAction Stop -WarningAction Stop 
-				outlog "OK"
+				outlog "OK" "DEBUG"
 			} catch {
 				$ErrorMessage = $_.Exception.Message
-				outlog "EXCEPTION: $ErrorMessage"
-				outlog "Waiting 120 seconds and trying again"
+				outlog "EXCEPTION: $ErrorMessage" "ERROR"
+				outlog "Waiting 120 seconds and trying again" "DEBUG"
 				Start-Sleep 120 
 				Try {
 					Set-Service LTService -startupType Automatic -ErrorAction Stop -WarningAction Stop 
-					outlog "OK"
+					outlog "OK" "DEBUG"
 				} catch {
 					$ErrorMessage = $_.Exception.Message
-					outlog "EXCEPTION: $ErrorMessage"
-					outlog "Service LTService is broken. Calling Reinstall"
+					outlog "EXCEPTION: $ErrorMessage" "ERROR"
+					outlog "Service LTService is broken. Calling Reinstall" "WARN"
 					Reinstall
 				}
 			}
 		} else {
-			outlog "LTService already set to Auto start"
+			outlog "LTService already set to Auto start" "DEBUG"
 		}
 
-		outlog "Checking LTService is Running"
+		outlog "Checking LTService is Running" "DEBUG"
 		If ((Get-Service LTService -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).Status -ne "Running") { 
-			outlog "LTService is not Running. Attempting to start it."
+			outlog "LTService is not Running. Attempting to start it." "WARN"
 			Try {
 				Start-Service LTService -ErrorAction Stop -WarningAction Stop  
 			} catch {
 				$ErrorMessage = $_.Exception.Message
-				outlog "EXCEPTION: $ErrorMessage"
-				outlog "Waiting 120 seconds and trying again"
+				outlog "EXCEPTION: $ErrorMessage" "ERROR"
+				outlog "Waiting 120 seconds and trying again" "DEBUG"
 				Start-Sleep 120 
 				Try {
 					Start-Service LTService -ErrorAction Stop -WarningAction Stop  
 				} catch {
 					$ErrorMessage = $_.Exception.Message
-					outlog "EXCEPTION: $ErrorMessage"
-					outlog "Service LTService is would not start. Calling Reinstall"
+					outlog "EXCEPTION: $ErrorMessage" "ERROR"
+					outlog "Service LTService is would not start. Calling Reinstall" "WARN"
 					Reinstall
 				}
 			}
 		} else {
-			outlog "LTService already Running"
+			outlog "LTService already Running" "INFO"
 		}
 
 		outlog "Checking LTSvcMon is set to Auto start"
 		If (((Get-WmiObject -Class Win32_Service -Property StartMode -Filter "Name='LTSvcMon'").StartMode) -ne "Auto") { 
-			outlog "LTSvcMon is not set to Auto start. Attempting to set it to Auto"
+			outlog "LTSvcMon is not set to Auto start. Attempting to set it to Auto" "WARN"
 			Try {
 				Set-Service LTSvcMon -startupType Automatic -ErrorAction Stop -WarningAction Stop 
-				outlog "OK"
+				outlog "OK" "DEBUG"
 			} catch {
 				$ErrorMessage = $_.Exception.Message
-				outlog "EXCEPTION: $ErrorMessage"
-				outlog "Waiting 120 seconds and trying again"
+				outlog "EXCEPTION: $ErrorMessage" "ERROR"
+				outlog "Waiting 120 seconds and trying again" "DEBUG"
 				Start-Sleep 120 
 				Try {
 					Set-Service LTSvcMon -startupType Automatic -ErrorAction Stop -WarningAction Stop 
 					outlog "OK"
 				} catch {
 					$ErrorMessage = $_.Exception.Message
-					outlog "EXCEPTION: $ErrorMessage"
-					outlog "Service LTSvcMon is broken. Calling Reinstall"
+					outlog "EXCEPTION: $ErrorMessage" "ERROR"
+					outlog "Service LTSvcMon is broken. Calling Reinstall" "WARN"
 					Reinstall
 				}
 			}
 		} else {
-			outlog "LTSvcMon already set to Auto start"
+			outlog "LTSvcMon already set to Auto start" "DEBUG"
 		}
 
 		outlog "Checking LTSvcMon is Running"
 		If ((Get-Service LTSvcMon -ErrorAction SilentlyContinue -WarningAction SilentlyContinue).Status -ne "Running") { 
-			outlog "LTSvcMon is not Running. Attempting to start it."
+			outlog "LTSvcMon is not Running. Attempting to start it." "WARN"
 			Try {
 				Start-Service LTSvcMon -ErrorAction Stop -WarningAction Stop
 				outlog "OK"  
 			} catch {
 				$ErrorMessage = $_.Exception.Message
-				outlog "EXCEPTION: $ErrorMessage"
-				outlog "Waiting 120 seconds and trying again"
+				outlog "EXCEPTION: $ErrorMessage" "ERROR"
+				outlog "Waiting 120 seconds and trying again" "DEBUG"
 				Start-Sleep 120 
 				Try {
 					Start-Service LTSvcMon -ErrorAction Stop -WarningAction Stop  
-					outlog "OK"
+					outlog "OK" "DEBUG"
 				} catch {
 					$ErrorMessage = $_.Exception.Message
-					outlog "EXCEPTION: $ErrorMessage"
-					outlog "Service LTSvcMon is would not start. Calling Reinstall"
+					outlog "EXCEPTION: $ErrorMessage" "ERROR"
+					outlog "Service LTSvcMon would not start. Calling Reinstall" "WARN"
 					Reinstall
 				}
 			}
 		} else {
-			outlog "LTSvcMon already Running"
+			outlog "LTSvcMon already Running" "DEBUG"
 		}
 		outlog "### Labtech Agent checks completed OK. Enjoy the rest of your uptime!"
     }
